07c38f231bc36b32a06546a42a1437080ffffcab
AnnasWRamadhan <45087352+AWRamadhan@users.noreply.github.com>
2020-01-02 22:21:43 +0700

    part1

--- offline-quickstart-lab/app/service-worker.js
+++ offline-quickstart-lab/app/service-worker.js
@@ -1,1 +1,34 @@
+const cacheName = 'chace-v1';
+const precacheResources = [
+    '/',
+    'index.html',
+    'styles/main.css',
+    '/images/space1.jpg',
+    '/images/space2.jpg',
+    '/images/space3.jpg'
+]
 
+self.addEventListener('install', event => {
+    console.log('Install begin');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cahce => {
+                return cahce.addAll(precacheResources);
+            })
+    );
+});
+
+self.addEventListener('active', event => {
+    console.log('installing...')
+});
+
+self.addEventListener('fetch', event => {
+    console.log(event.request.url);
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        }))
+});
